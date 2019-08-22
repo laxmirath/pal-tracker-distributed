2e13de299cd3080fefbe825542630cc025533b95
richa_verma2 <richa_verma2@dell.com>
2019-08-22 20:06:35 +0530

    Eureka integration

--- integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
+++ integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.fail;
 import static test.pivotal.pal.tracker.support.MapBuilder.envMapBuilder;
 
-public class ApplicationServer {
+public class    ApplicationServer {
 
     private final String jarPath;
     private final String port;
@@ -37,7 +37,9 @@ public class ApplicationServer {
 
         start(envMapBuilder()
             .put("SPRING_DATASOURCE_URL", dbUrl)
-            .put("REGISTRATION_SERVER_ENDPOINT", "http://localhost:8883")
+                .put("EUREKA_CLIENT_ENABLED", "false")
+                .put("RIBBON_EUREKA_ENABLED", "false")
+                .put("REGISTRATION_SERVER_RIBBON_LISTOFSERVERS", "http://localhost:8883")
             .build()
         );
     }
